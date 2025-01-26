87c7a921992d0d14b99615ba46256887cf0b3cb9
jor-eldevera <jorell.devera@gmail.com>
2025-01-25 23:05:21 -0800

    Import and hook up routers, hook up ejs

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const app = express();
 
 const PORT = 8080;
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+// Routers
+const homeRouter = require("./routes/HomeRouter");
+const categoryRouter = require("./routes/CategoryRouter");
+
+// hooking up ejs
+const path = require("node:path");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// middleware to parse new message form data
+app.use(express.urlencoded({ extended: true }));
+
+// hooking up routers
+app.use("/", homeRouter);
+app.use("/categories", categoryRouter);
 
 app.listen(PORT);
