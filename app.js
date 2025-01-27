b09baaf044db3f823bb8d9578de24f22f40ea653
jor-eldevera <jorell.devera@gmail.com>
2025-01-27 01:13:11 -0800

    fix: Allow the usage of external css files

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
@@ -11,6 +12,7 @@ const categoryRouter = require("./routes/CategoryRouter");
 const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/views"));
 
 // middleware to parse new message form data
 app.use(express.urlencoded({ extended: true }));
