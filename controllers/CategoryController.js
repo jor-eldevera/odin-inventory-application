234d1444d5055f462768b633dfebec40b5049da8
jor-eldevera <jorell.devera@gmail.com>
2025-01-26 23:50:15 -0800

    Display allCategories.ejs at /categories

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,1 +1,9 @@
 const db = require("../db/queries");
+
+async function getAllCategoriesPage(req, res) {
+    res.render("allCategories");
+}
+
+module.exports = {
+    getAllCategoriesPage
+}
--- routes/CategoryRouter.js
+++ routes/CategoryRouter.js
@@ -4,4 +4,6 @@ const categoryRouter = Router();
 
 const controller = require("../controllers/CategoryController");
 
+categoryRouter.get("/", controller.getAllCategoriesPage);
+
 module.exports = categoryRouter;
